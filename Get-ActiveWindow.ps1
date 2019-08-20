ebc9fd2ecbd8d73c1542b0d0ea01ea1ecd88fc2a
Ryan Yates <ryan.Yates@kilasuit.org>
2019-08-20 04:52:49 +0100

    Changing Sleep duration as it's a bit too frequent

---
+++ Get-ActiveWindow.ps1
@@ -0,0 +1,58 @@
+#Get-GetActiveWindow
+$code = @'
+    [DllImport("user32.dll")]
+     public static extern IntPtr GetForegroundWindow();
+'@
+$NonInteractiveDefaultProcesses = @(
+    'ApplicationFrameHost',
+    'backgroundTaskHost'
+    'CodeHelper',
+    'CompPkgSrv',
+    'ctfmon',
+    'dllhost',
+    'Docker',
+    'dptf_helper',
+    'FileCoAuth',
+    'gpg-agent',
+    'OneDrive',
+    'ONENOTEM',
+    'RemindersServer',
+    'RuntimeBroker',
+    'SCNotification'
+    'SearchUI',
+    'SearchProtocolHost',
+    'SettingSyncHost',
+    'SecurityHealthSystray',
+    'ShellExperienceHost',
+    'sihost',
+    'SkypeApp',
+    'SkypeBackgroundHost',
+    'SkypeBridge',
+    'smartscreen',
+    'SpeechRuntime',
+    'StartMenuExperienceHost',
+    'SurfaceDTX', 
+    'SystemSettingsBroker',
+    'svchost', 
+    'TabTip',
+    'taskhostw', 
+    'vsls-agent'
+    'WindowsInternal.ComposableShell.Experiences.TextInput.InputApp',
+    'WinStore.App',
+    'YourPhone'
+    )
+    
+    
+    
+    )
+
+Add-Type $code -Name Utils -Namespace Win32
+while (1) {
+        
+    $hwnd = [Win32.Utils]::GetForegroundWindow()
+    Get-Process -IncludeUserName | 
+    Where-Object { ($_.mainWindowHandle -eq $hwnd) -and ($_.userName -match $env:USERNAME) -and ($_.ProcessName -notin $NonInteractiveDefaultProcesses) } | 
+    Select-Object processName, MainWindowTitle, MainWindowHandle, @{name = 'CurrentTime' ; expression = { [datetime]::Now.ToLongTimeString() } }, @{name = 'CurrentDate' ; expression = { [datetime]::Now.ToShortDateString() } } |  
+    Export-Csv -Path C:\tracker\ActiveWindow.csv -Append
+    Start-Sleep -Seconds 1
+} 
